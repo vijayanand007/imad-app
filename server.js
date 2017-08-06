54aca0758fd10e7106b94f52cd97d62fe467c090
vijay anand <xookr.india@gmail.com>
2017-08-06 19:48:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-   res.sendFIle(path.join(__dirname, 'ui', 'article-one.html')); 
+   res.send('article one is coming on its way')) 
+});
+app.get('/article-two', function (req, res) {
+   res.send('article two is coming on its way')) 
+});
+app.get('/article-three', function (req, res) {
+   res.send('article three is coming on its way')) 
 });
 app.get('/ui/style.css', function (req, res) {
-  res.send('article one will be servered here in a min');
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
